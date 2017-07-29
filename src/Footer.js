31c056368e008214b7bddfb2b570fd2ee6efbdba
mysterion <sv.hornung@gmx.de>
2017-07-29 14:32:42 +0200

    created new react-app

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+          <p className="Footer-text">This is a footer created by Sven Hornung</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
